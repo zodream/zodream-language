3399864a577faaa8454acfd8708a2b1b9ef147bb
ZoDream <648383079@qq.com>
2023-07-28 21:48:01 +0800

    update

--- build/src/tokenizer/reader.rs
+++ build/src/tokenizer/reader.rs
@@ -1,3 +1,52 @@
 mod tokenizer {
-    
+    use std::str::Chars;
+
+    struct Tokenizer {
+        source: Chars,
+    }
+
+    impl Tokenizer {
+        fn new(input: &str) -> Tokenizer {
+            Tokenizer{source: input.chars()}
+        }
+
+        fn nextToken() -> Option<String> {
+            match self.chars.next() {
+                Some(' ') => {
+                    loop {
+                        match self.chars.next() {
+                            Some(_) => {},
+                            None => return None,
+                        }
+                    }
+                }
+                Some('{') => return Some("{".to_string()),
+                Some('}') => return Some("}".to_string()),
+                Some(')') => return Some("}".to_string()),
+                Some('(') => return Some("(".to_string()),
+                Some(',') => return Some(",".to_string()),
+                Some('"') => {
+                    let mut result = String::new();
+                    while let Some(c) = self.chars.next() {
+                        result.push(c);
+                        if c == '"' {
+                            return Some(result);
+                        }
+                    }
+                    None
+                }
+                Some(c) => {
+                    let mut result = String::new();
+                    result.push(c);
+                    while let Some(c) = self.chars.next() {
+                        if c == ' ' || c == ',' || c == '"' || c == '{' || c == '}' || c == '(' || c == ')' {
+                            return Some(result);
+                        }
+                        result.push(c);
+                    }
+                    Some(result)
+                }
+            }
+        }
+    }
 }
